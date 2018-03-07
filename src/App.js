de88c80751e3ca7c3a9158ffbb67efe2d107fe4c
Marc <recatala.marc@gmail.com>
2018-03-07 18:05:49 +0100

    Added basic chat interface and logic to send messages to the backend

--- src/App.js
+++ src/App.js
@@ -3,9 +3,26 @@ import openSocket from "socket.io-client";
 const socket = openSocket("http://localhost:8000");
 
 class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			message: ""
+		};
+	}
+	sendMessage = () => {
+		socket.emit("newMessage", this.state.message );
+		console.log("Sended");
+	};
+
+	handleChange = (e) => {
+		this.setState({ [ e.target.name ]: e.target.value });
+	};
+
   render() {
     return (
       <div>
+				<input name="message" onChange={this.handleChange} />
+				<button onClick={this.sendMessage}>Send message</button>
       </div>
     );
   }
