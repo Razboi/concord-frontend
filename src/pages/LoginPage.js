20b70773a3d91044c4d34cfda6d49095453afec3
Marc <recatala.marc@gmail.com>
2018-03-15 22:11:26 +0100

    Worked on implementing passportjs

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,17 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Container, Button } from "semantic-ui-react";
+import axios from "axios";
 
 class LoginPage extends React.Component {
+
+	handleOauth = () => {
+		axios.get("http://localhost:8000/oauth/google")
+		.then( res => console.log( res ) )
+		.catch( err => console.log( err ) );
+	};
 
 	render() {
 		return (
 			<Container>
 				<h2>Login Page</h2>
 				<Link to="/">Back to home</Link>
-				<a href="http://localhost:8000/oauth/google">
-					<Button primary content="Login with Google+" />
-				</a>
+
+				<Button onClick={this.handleOauth} primary content="Login with Google+" />
 			</Container>
 		);
 	}
