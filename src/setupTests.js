98dedf119d21fdd363aa5febf770033252ac8678
Marc <recatala.marc@gmail.com>
2018-03-29 22:14:35 +0200

    Added undefined XMLHttpRequest for testing to avoid network errors

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,7 +1,10 @@
-// creates a fake localStorage before running tests
+// creates custom global variables before running tests to avoid errors
 const localStorageMock = {
 	getItem: jest.fn(),
 	setItem: jest.fn(),
 	clear: jest.fn()
 };
+// to avoid localStorage errors
 global.localStorage = localStorageMock;
+// to avoid network error with axios
+global.XMLHttpRequest = undefined;
